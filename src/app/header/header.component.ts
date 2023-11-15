48ba20b7f4c8e3426ca096a23af4f0730bdeb774
mortib20 <martin.paul-3277@web.de>
2023-11-15 20:34:08 +0100

    fixed header links on mobile

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Settings } from '../settings';
 
 @Component({
@@ -8,13 +8,37 @@ import { Settings } from '../settings';
 })
 export class HeaderComponent {
   public linksOpen = false;
+  public currentWidth: number;
+  public currentHeight: number;
 
   constructor(public settings: Settings) {
+    this.currentWidth = window.innerWidth;
+    this.currentHeight = window.innerHeight;
   }
 
   changeLinksOpen() {
     this.linksOpen = !this.linksOpen;
-    console.log("LOL");
+  }
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.currentWidth = event.target.currentWidth;
+    this.currentHeight = event.target.currentHeight;
+  }
+
+  enableLinksOpen() {
+    if (this.currentWidth <= 600) {
+      return;
+    }
+
+    this.linksOpen = true;
+  }
+
+  disableLinksOpen() {
+    if (this.currentWidth <= 600) {
+      return;
+    }
+
+    this.linksOpen = false;
   }
 }

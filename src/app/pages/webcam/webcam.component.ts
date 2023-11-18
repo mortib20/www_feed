109f4d159ff0d285ab7598f58a3fcf8c2bd76da5
Martin Paul <martin.paul-3277@web.de>
2023-11-18 14:15:44 +0100

    added webcam api

--- src/app/pages/webcam/webcam.component.ts
+++ src/app/pages/webcam/webcam.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WebcamAPIService } from './services/WebcamAPI.service';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-webcam',
   templateUrl: './webcam.component.html',
   styleUrls: ['./webcam.component.css']
 })
-export class WebcamComponent {
+export class WebcamComponent implements OnInit {
+  private _currentImages: BehaviorSubject<string[]> = new BehaviorSubject<string[]>([]);
+  public currentImages$ = this._currentImages.asObservable();
 
+  constructor(private api: WebcamAPIService) {
+  }
+  ngOnInit(): void {
+    this.api
+      .getImagesByDate(new Date(Date.now()))
+      .subscribe(s => {
+        this._currentImages.next(s);
+        console.log(s);
+      });
+  }
+
+  public appendWebcamLink(file: string) {
+    return this.api.webcamLink + 'img/' + file;
+  }
 }

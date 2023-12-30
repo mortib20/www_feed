a7f6a928707041b8a3fbc7d8f7303773fe0f3f80
Martin Paul <martin.paul-3277@web.de>
2023-12-30 10:10:33 +0100

    changed socket.io endpoint

--- src/app/settings.ts
+++ src/app/settings.ts
@@ -1,7 +1,7 @@
 export class Settings {
   public readonly title = '💺MP-EDQM01';
   public readonly tar1090 = 'https://dyn2.martpaul.de/tar1090/';
-  public readonly acarflow = 'wss://dyn2.martpaul.de:22001';
+  public readonly acarflow = 'wss://dyn2.martpaul.de';
   public readonly acarflowOutputs = ['acarsdec', 'dumphfdl', 'dumpvdl2', 'jaero'];
   public readonly webcamLink = 'https://dyn2.martpaul.de/webcamapi/';
 }
